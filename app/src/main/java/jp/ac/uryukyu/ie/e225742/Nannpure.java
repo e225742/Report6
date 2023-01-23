dc0aa29a7ad1469bdc22c459e6ce494e71b28423
Takuyuki Kawakami <e225742@ie.u-ryukyu.ac.jp>
2023-01-23 23:47:35 +0900

    Report6(2.0)

--- app/src/main/java/jp/ac/uryukyu/ie/e225742/Nannpure.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e225742/Nannpure.java
@@ -1,33 +1,60 @@
 package jp.ac.uryukyu.ie.e225742;
-import java.util.ArrayList;
-//import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Random;
 
-/**
- * ９×９のナンプレ完成表を作るクラス
- */
 public class Nannpure{
-    /**
-     * ９マスに数字を入れた塊を９個用意する
-     */
-    ArrayList<Integer> list = new ArrayList<>();
-    Set<Integer> integerSet = new HashSet<>();
-    int i = 0, j = 0, x = 0, count = 0;
-    int[][] num = new int[9][9];
-    public void NannpureList(){
-        for (i = 1; i <= 9; i++){
-            list.add(i);
-        }
-    }
+    int i, j, k, l, check = 0, count = 0, tmp;
+    int a[][] = new int[9][9];
+    Random rnd = new Random();
+    int ran;
+    boolean A = false;
     public void CompletionList(){
-        System.out.println("完成表");
-        for (i = 0; i < 9; i++){
-            for (j = 0; j < 9; j++){
-                System.out.printf("%3d", num[i][j]);
+        while(A == false){
+            A = true;
+            for(i = 0; i < 9; i++)
+            for(j = 0; j < 9; j++)
+            a[i][j] = 0;
+            count = 0;
+            
+            for(i = 0; i < 9; i++){
+                for(j = 0; j < 9; j++){
+                    ran = rnd.nextInt(9);
+                    tmp = ran + 1;
+                    check = 0;
+                    
+                    for(k = 0; k < j; k++)
+                    if(a[i][k] == tmp)
+                    check = 1;
+                    for(k = 0; k < i; k++)
+                    if(a[k][j] == tmp)
+                    check = 1;
+
+                    for(k = (i/3)*3; k < (i/3)*3+3; k++)
+                    for(l = (j/3)*3; l < (j/3)*3+3; l++)
+                    if(a[k][l] == tmp)
+                    check = 1;
+
+                    if(check == 0)
+                    a[i][j] = tmp;
+                    if(check == 1)
+                    j--;
+
+                    if(count > 50000){
+                        A = false; break;
+                    }
+                    count++;
+                }
+                count = 0;
             }
-            System.out.println();
         }
-        System.out.println("実行回数" + count + "回");
+        System.out.println("完成表");
+        for(i = 0; i < 9; i++){
+            for(j = 0; j < 9; j++){
+                if(a[i][j] < 10){
+                    System.out.print(" ");
+                }
+                System.out.print(a[i][j]);
+            }
+            System.out.print("\n");
+        }
     }
 }
